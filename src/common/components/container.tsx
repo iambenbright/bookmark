5b656825eb74a36f18dac9e9c6891abd91b8eb4a
iambenbright <yankeybenneth@gmail.com>
2021-01-10 18:54:31 +0000

    chore: refactor styles

--- src/common/components/container.tsx
+++ src/common/components/container.tsx
@@ -3,13 +3,19 @@ import Box from '@material-ui/core/Box';
 interface ContainerProps {
   children?: React.ReactNode;
   style?: object;
+  className?: string;
 }
 
 export default function Container(props: ContainerProps) {
-  const { children, style, ...restProps } = props;
+  const { children, style, className, ...restProps } = props;
 
   return (
-    <Box px={{ xs: 2, md: 6, lg: 10 }} style={style} {...restProps}>
+    <Box
+      px={{ xs: 2, md: 6, lg: 10 }}
+      className={className}
+      style={style}
+      {...restProps}
+    >
       {children}
     </Box>
   );

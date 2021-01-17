1a18f9183d6f09448f87fb707c3b75e299476f43
iambenbright <yankeybenneth@gmail.com>
2021-01-17 02:37:12 +0000

    chore: removed productionPrefix

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,22 +1,8 @@
 import React from 'react';
 
-// material-ui
-import {
-  StylesProvider,
-  createGenerateClassName,
-} from '@material-ui/core/styles';
-
 // pages
 import { Landing } from '../../pages/landing';
 
-const generateClassNames = createGenerateClassName({
-  productionPrefix: 'bmk',
-});
-
 export default function App() {
-  return (
-    <StylesProvider generateClassName={generateClassNames}>
-      <Landing />
-    </StylesProvider>
-  );
+  return <Landing />;
 }
